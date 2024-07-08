7ac67199fd0e90125d118c8279b450215abb4022
Fraser Martin <ffm23@ic.ac.uk>
2024-07-08 14:13:20 +0100

    added prior

--- code/losses/nll.py
+++ code/losses/nll.py
@@ -7,8 +7,16 @@ import torch
 def NLL(mean, log_var, targets):
     # Ensure positive variance using the softplus function
     var = torch.exp(log_var)
-    nll_data = 0.5 * ((targets - mean) ** 2 / var)
-    nll_var = 0.5 * torch.log(var)
-    nll = nll_var + nll_data
-    return nll.mean()
+    nll_data = (0.5 * ((targets - mean) ** 2 / var)).sum()
+    nll_var = (0.5 * torch.log(var)).sum()
+    nll =  nll_data + nll_var 
+
+    #prior
+    prior = torch.distributions.Gamma(
+        torch.tensor(1).to(mean.device),
+        torch.tensor(1).to(mean.device)
+    )
+    log_prior = prior.log_prob(1./var).sum()
+    total_nll = nll - log_prior
+    return total_nll
  
