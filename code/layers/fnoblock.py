443498027afad5efd48985c944290d546342aad5
Fraser Martin <ffm23@ic.ac.uk>
2024-07-19 21:14:45 +0100

    added comments that can add extra MLP to process

--- code/layers/fnoblock.py
+++ code/layers/fnoblock.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from layers.spectralconv import SpectralConv2D
-
+from layers.mlp2d import MLP2D
 
 class FNOBlock2D(nn.Module):
     def __init__(
@@ -19,6 +19,7 @@ class FNOBlock2D(nn.Module):
         self.non_linearity= non_linearity
 
         self.W = nn.Conv2d(in_channels=self.in_ch, out_channels= self.out_ch, kernel_size=1)
+        #self.mlp = MLP2D(in_channels=self.out_ch, out_channels=self.out_ch, hidden_channels=self.out_ch)
         self.spectral = SpectralConv2D(n_modes = self.n_modes,in_ch=self.in_ch, out_ch=self.out_ch)
 
     def forward(self,x):
