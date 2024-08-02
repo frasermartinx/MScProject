9f3c522a21c6e232bb7e949c9c2cd7bc021d9464
Fraser Martin <ffm23@ic.ac.uk>
2024-08-02 14:53:16 +0100

    Polar version with indexing parameter for meshgrid

---
+++ code/plotting/results_plotter_polar.py
@@ -0,0 +1,40 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def plotter(input,prediction,solution,r_range,theta_range,idx = False):
+
+    R,Theta = np.meshgrid(r_range, theta_range)
+
+    X = R * np.cos(Theta)
+    Y = R * np.sin(Theta)
+    fig, axs = plt.subplots(nrows = 1, ncols = 3,gridspec_kw={'width_ratios': [1, 1, 1]})
+    fig.set_figwidth(20)
+
+
+    fig0 = axs[0].pcolormesh(X,Y,input.cpu().detach().numpy())
+    fig.colorbar(fig0)
+    axs[0].set_title("Input Function")
+    axs[0].set_xlabel("x")
+    axs[0].set_ylabel("y")
+
+    #prediction
+    fig1 = axs[1].pcolormesh(X,Y,prediction.cpu().detach().numpy())
+    fig.colorbar(fig1)
+    axs[1].set_title("Model Prediction")
+    axs[1].set_xlabel("x")
+    axs[1].set_ylabel("y")
+
+    if idx:
+        R,Theta = np.meshgrid(r_range, theta_range, indexing = "ij")
+
+        X = R * np.cos(Theta)
+        Y = R * np.sin(Theta)
+
+    #actual
+    fig2 = axs[2].pcolormesh(X,Y,solution.cpu())
+    fig.colorbar(fig2)
+    axs[2].set_title("Numerical Solution")
+    axs[2].set_xlabel("x")
+    axs[2].set_ylabel("y")
+    plt.show()
---
+++ code/plotting/results_plotter_polar_3D.py
@@ -0,0 +1,43 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+def plotter_3D(input,prediction,solution,r_range,theta_range, idx = False):
+
+    R,Theta = np.meshgrid(r_range, theta_range)
+
+    X = R * np.cos(Theta)
+    Y = R * np.sin(Theta)
+    fig, axs = plt.subplots(1, 3, figsize=(14, 6))
+
+    ax = fig.add_subplot(131, projection='3d')
+    ax.plot_surface(X, Y, input, cmap='viridis')
+    ax.set_title("Input")
+    ax.set_xlabel("x")
+    ax.set_ylabel("y")
+    ax.set_zlabel("u")
+
+    ax = fig.add_subplot(132, projection='3d')
+    ax.plot_surface(X, Y, prediction, cmap='viridis')
+    ax.set_title("Prediction")
+    ax.set_xlabel("x")
+    ax.set_ylabel("y")
+    ax.set_zlabel("u")
+
+    if idx:
+
+        R,Theta = np.meshgrid(r_range, theta_range, indexing = "ij")
+
+        X = R * np.cos(Theta)
+        Y = R * np.sin(Theta)
+
+    ax = fig.add_subplot(133, projection='3d')
+    ax.plot_surface(X, Y, solution, cmap='viridis')
+    ax.set_title("Solution")
+    ax.set_xlabel("x")
+    ax.set_ylabel("y")
+    ax.set_zlabel("u")
+
+    plt.show()
+
+    
