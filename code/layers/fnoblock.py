b7a47df5bceae45a2cc79182663df59faf44294a
Fraser Martin <ffm23@ic.ac.uk>
2024-08-16 03:09:05 +0100

    Added comment so I can switch between my version and neurlop version of Sconv

--- code/layers/fnoblock.py
+++ code/layers/fnoblock.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 from layers.spectralconv import SpectralConv2D
+from neuralop.layers.spectral_convolution import SpectralConv
 from layers.mlp2d import MLP2D
 
 class FNOBlock2D(nn.Module):
@@ -20,7 +21,8 @@ class FNOBlock2D(nn.Module):
 
         self.W = nn.Conv2d(in_channels=self.in_ch, out_channels= self.out_ch, kernel_size=1)
         #self.mlp = MLP2D(in_channels=self.out_ch, out_channels=self.out_ch, hidden_channels=self.out_ch)
-        self.spectral = SpectralConv2D(n_modes = self.n_modes,in_ch=self.in_ch, out_ch=self.out_ch)
+        #self.spectral = SpectralConv2D(n_modes = self.n_modes,in_ch=self.in_ch, out_ch=self.out_ch)
+        self.spectral = SpectralConv(in_channels=self.in_ch, out_channels=self.out_ch, n_modes=self.n_modes)
 
     def forward(self,x):
         h = self.W(x)
