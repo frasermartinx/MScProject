83b8971ad9309a4d4874cfca9d85d2c37e27e33e
Fraser Martin <ffm23@ic.ac.uk>
2024-08-18 02:44:44 +0100

    Removed batch norm

--- code/layers/fnoblock.py
+++ code/layers/fnoblock.py
@@ -11,26 +11,19 @@ class FNOBlock2D(nn.Module):
         n_modes,
         in_ch,
         out_ch,
-        non_linearity = F.gelu,
-        normalization = False
+        non_linearity = F.gelu
         ):
         super().__init__()
         self.n_modes = n_modes
         self.in_ch = in_ch
         self.out_ch = out_ch
         self.non_linearity= non_linearity
-        self.normalization = normalization
 
         self.W = nn.Conv2d(in_channels=self.in_ch, out_channels= self.out_ch, kernel_size=1)
         #self.mlp = MLP2D(in_channels=self.out_ch, out_channels=self.out_ch, hidden_channels=self.out_ch)
         #self.spectral = SpectralConv2D(n_modes = self.n_modes,in_ch=self.in_ch, out_ch=self.out_ch)
         self.spectral = SpectralConv(in_channels=self.in_ch, out_channels=self.out_ch, n_modes=self.n_modes)
-        if normalization:
-            self.bn = nn.BatchNorm2d(self.out_ch)
 
     def forward(self,x):
         h = self.W(x)
-        out = h + self.spectral(x)
-        if self.normalization:
-            out = self.bn(out)
-        return self.non_linearity(out)
+        return self.non_linearity(h + self.spectral(x))
